120a378155c7b2f0e414a836e756807a5002e03c
Jia Wang <daisy-a.119@hotmail.com>
2023-12-31 19:09:04 -0500

    setting up the scaffolding

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
-console.log('test')
+/*-------------------------------- Constants --------------------------------*/
+const choices = ["stand","hit"]
+
+
+
+/*-------------------------------- Variables --------------------------------*/
+let msg, playerHand, dealerHand, winner
+
+
+/*------------------------ Cached Element References ------------------------*/
+const messageEl = document.getElementById("message")
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+document.getElementById('play').addEventListener('click', play)
+
+
+/*-------------------------------- Functions --------------------------------*/
+function play(e) {
+  console.log(e)
+}
