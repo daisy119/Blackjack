c5374d8cb6208a01f330332a91af210e6dd36032
Jia Wang <daisy-a.119@hotmail.com>
2024-01-02 11:54:00 -0500

    add drawPlayerNew function

--- js/app.js
+++ js/app.js
@@ -8,6 +8,8 @@ let msg, bet, totalAmount, winner, playerTotal, dealerTotal, cardToRemove
 let deck1 = []
 let playerHand = []
 let dealerHand = []
+let playerHandNew = []
+let dealerHandNew = []
 
 
 /*------------------------ Cached Element References ------------------------*/
@@ -23,14 +25,13 @@ const messageEl = document.getElementById("message")
 // messageEl.innerHTML = 'hello'
 
 const playBtn =document.querySelector('#play')
-console.log(playBtn)
 const hitBtn =document.querySelector('#hit')
-console.log(hitBtn)
 const standBtn =document.querySelector('#stand')
-console.log(standBtn)
+// console.log(standBtn)
 
 /*----------------------------- Event Listeners -----------------------------*/
 playBtn.addEventListener('click', function() {console.log('who is playing cards')})
+hitBtn.addEventListener('click',drawPlayerNew)
 
 /*-------------------------------- Functions --------------------------------*/
 init()
@@ -38,13 +39,20 @@ init()
 function init() {
   deck1 = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"]
 
-
-
-
   drawDealer()
   drawDealer()
   drawPlayer()
   drawPlayer()
+}
+
+function drawPlayerNew() {
+  if (deck1.length > 0){
+    let randIdx = Math.floor(Math.random()*deck1.length)
+    let cardPicked = deck1.splice(randIdx, 1)[0]
+    playerHandNew.push(cardPicked)
+    console.log("🚀 ~ file: app.js:39 ~ playerHandNew:", playerHandNew)
+    // renderPlayer(cardPicked)
+  }
 }
 
 function drawPlayer() {
