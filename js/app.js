3777a3d8af5db447dc7f4cb8dda1315960c1bd89
Jia Wang <daisy-a.119@hotmail.com>
2024-01-02 00:25:45 -0500

    finish the first two draw for player

--- js/app.js
+++ js/app.js
@@ -29,6 +29,7 @@ function init() {
   deck1 = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"]
 
   drawPlayer()
+  drawPlayer()
 }
 
 function drawPlayer() {
@@ -39,24 +40,35 @@ function drawPlayer() {
     render(cardPicked)
   }
 }
+ //console.log(playerHand)
 
 function render(cardPicked) {
   if (playerHand.length === 1) {  
-    p2El.classList.remove("outline")
+    p1El.classList.add(cardPicked) 
   }
-  if (playerHand.length > 1) {  
-    deck2El.classList.remove(cardToRemove)
+  else if (playerHand.length ===2) {
+    p2El.classList.add(cardPicked)
   }
-  cardToRemove = cardPicked  
-  p2El.classList.add(cardPicked)  
-  if (deck1.length === 26) {  
-    p2El.classList.add("shadow");
-    p1El.classList.remove("shadow");
-  }
-  if (deck1.length === 0) {  
-    p1El.classList.add("outline");
-    p1El.classList.remove("back-blue");
-  }
+
+
+
+  // if (playerHand.length === 1) {  
+  //   p2El.classList.remove("outline")
+  // }
+  // if (playerHand.length > 1) {  
+  //   p2El.classList.remove(cardToRemove)
+  // }
+  // cardToRemove = cardPicked  
+
+  // p1El.classList.add()
+  // if (deck1.length === 26) {  
+  //   p2El.classList.add("shadow");
+    // p1El.classList.remove("shadow");
+  // }
+  // if (deck1.length === 0) {  
+  //   p1El.classList.add("outline");
+  //   p1El.classList.remove("back-blue");
+  // }
 }
 
 function play(e) {
