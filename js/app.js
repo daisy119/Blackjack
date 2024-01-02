3ea2d5a3c6b173c6509a4b0e615e5c8c91318d93
Jia Wang <daisy-a.119@hotmail.com>
2024-01-02 00:38:02 -0500

    finish the board setting

--- js/app.js
+++ js/app.js
@@ -28,6 +28,8 @@ init()
 function init() {
   deck1 = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"]
 
+  drawDealer()
+  drawDealer()
   drawPlayer()
   drawPlayer()
 }
@@ -37,38 +39,37 @@ function drawPlayer() {
     let randIdx = Math.floor(Math.random()*deck1.length)
     let cardPicked = deck1.splice(randIdx, 1)[0]
     playerHand.push(cardPicked)
-    render(cardPicked)
+    renderPlayer(cardPicked)
   }
 }
  //console.log(playerHand)
 
-function render(cardPicked) {
+function renderPlayer(cardPicked) {
   if (playerHand.length === 1) {  
     p1El.classList.add(cardPicked) 
   }
   else if (playerHand.length ===2) {
     p2El.classList.add(cardPicked)
   }
+}
 
+function drawDealer() {
+  if (deck1.length > 0){
+    let randIdx = Math.floor(Math.random()*deck1.length)
+    let cardPicked = deck1.splice(randIdx, 1)[0]
+    dealerHand.push(cardPicked)
+    renderDealer(cardPicked)
+  }
+}
+ //console.log(playerHand)
 
-
-  // if (playerHand.length === 1) {  
-  //   p2El.classList.remove("outline")
-  // }
-  // if (playerHand.length > 1) {  
-  //   p2El.classList.remove(cardToRemove)
-  // }
-  // cardToRemove = cardPicked  
-
-  // p1El.classList.add()
-  // if (deck1.length === 26) {  
-  //   p2El.classList.add("shadow");
-    // p1El.classList.remove("shadow");
-  // }
-  // if (deck1.length === 0) {  
-  //   p1El.classList.add("outline");
-  //   p1El.classList.remove("back-blue");
-  // }
+function renderDealer(cardPicked) {
+  if (dealerHand.length === 1) {  
+    d1El.classList.add(cardPicked) 
+  }
+  else if (dealerHand.length ===2) {
+    d2El.classList.add(cardPicked)
+  }
 }
 
 function play(e) {
