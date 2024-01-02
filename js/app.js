87207d8fee03ae7053280d4c90ee340cd9430c48
Jia Wang <daisy-a.119@hotmail.com>
2024-01-02 11:42:09 -0500

    test a new method for card drawing

--- js/app.js
+++ js/app.js
@@ -17,8 +17,8 @@ let d2El = document.getElementById('deck-3')
 let d1El = document.getElementById('deck-4')
 
 const cardContainer =document.querySelector('#card-container')
-console.log("🚀 ~ file: app.js:20 ~  cardContainer:",  cardContainer)
-// cardContainer.innerHTML = '<p>sleepy</p>'
+// console.log("🚀 ~ file: app.js:20 ~  cardContainer:",  cardContainer)
+//  cardContainer.innerHTML = '<p>sleepy</p>'
 const messageEl = document.getElementById("message")
 // messageEl.innerHTML = 'hello'
 
@@ -30,13 +30,16 @@ const standBtn =document.querySelector('#stand')
 console.log(standBtn)
 
 /*----------------------------- Event Listeners -----------------------------*/
-playBtn.addEventListener('click', () => {console.log('who is playing cards')})
+playBtn.addEventListener('click', function() {console.log('who is playing cards')})
 
 /*-------------------------------- Functions --------------------------------*/
 init()
 
 function init() {
   deck1 = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"]
+
+
+
 
   drawDealer()
   drawDealer()
